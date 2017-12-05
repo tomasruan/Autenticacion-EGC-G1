61836a5996c3305602ae1f92fdb15a7081371395
Alejandro Hall <alejandropolvillo@gmail.com>
2017-12-05 11:51:04 +0100

    Añade la plantilla para el proyecto. Ref #15

--- login/controllers.py
+++ login/controllers.py
@@ -1,20 +1,29 @@
-from database import db, User, UserSchema
+from database import db, TableNameExample, TableNameExampleSchema
 from flask import Flask, redirect, url_for
 
 
 app = Flask(__name__)
 
+#Example of how to add something to database
+@app.route('/add')
+def add():
+    schema = TableNameExampleSchema()
 
-@app.route('/')
+    prueba = TableNameExample(title='Titulo')
+
+    db.session.add(prueba)
+    db.session.commit()
+
+    return schema.dump(prueba).data
+
+
+#Example of how to retrieve a table from database and return it as json
+@app.route('/prueba')
 def root():
-    users_schema = UserSchema(many=True)
-    all_users = User.query.all()
+    prueba_schema = TableNameExampleSchema(many=True)
+    prueba = TableNameExample.query.all()
 
-    return users_schema.dump(all_users).data
-
-
-
-
+    return prueba_schema.dump(prueba).data
 
 
 if __name__ == '__main__':
--- login/database.py
+++ login/database.py
@@ -1,32 +1,20 @@
-import click
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://user:user123@127.0.0.1/database_login'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:root123@127.0.0.1/votaciones_splc'
 
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
 
 
-@app.cli.command()
-def initdb():
-    db.create_all()
-    click.echo('Base de datos inicializada')
-
-
-class User(db.Model):
+class TableNameExample(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
+    title = db.Column(db.String(100))
 
 
-class UserSchema(ma.ModelSchema):
+class TableNameExampleSchema(ma.ModelSchema):
     class Meta:
-        model = User
-
-
-
-
+        model = TableNameExample
