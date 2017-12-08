73255ad1d80d99de00ab4df7198bd1d6a31a5c5e
Unknown <javi.santos.me@gmail.com>
2017-12-08 13:45:40 +0100

    Refactoriza código de verificación de Hash. Ref #14

--- login/wp_hash/test.py
+++ login/wp_hash/test.py
@@ -1,16 +1,21 @@
 from passlib.hash import phpass
-import wp_hash
+from wp_hash.hash import crypt_private
 
-# FORMA 1
+# OPCION 1
 # USANDO passlib
 # https://passlib.readthedocs.io/en/stable/narr/hash-tutorial.html#password-hash-examples
+print("OPCION 1")
+
 hash = phpass.hash("hola")
 print(hash)
 print(phpass.verify("hola", hash))
 
 
-# FORMA 2
-# USANDO wp_hash
+# OPCION 2
+# USANDO clase hash
 # https://gist.github.com/romke/61dd1313c6ab112186ac
-#hash = wp_hash.crypt_private("hola")
+#print("\n")
+#print("OPCION 2")
+
+#hash = crypt_private("hola")
 #print(hash)
