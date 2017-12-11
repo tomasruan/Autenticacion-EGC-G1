7d90941e4f84b6872993e438afe6c29dd63dabc9
tomasruan <hermanosruan@hotmail.com>
2017-12-11 11:51:55 +0100

    Añade el modelo para las cuentas de usuario. Ref #11

--- login/database.py
+++ login/database.py
@@ -10,10 +10,28 @@ db = SQLAlchemy(app)
 ma = Marshmallow(app)
 
 
+#Vamos a necesitar 2 modelos y 2 schemas (1 schema por cada modelo)
+
+
 class TableNameExample(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(100))
+    title = db.Column(db.String(100),)
 
+#Modelos de las tablas
+
+class user_account(db.Model):
+    id = db.Column(db.Integer, nullable = False, primary_key=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    password = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(100), nullable=False)
+    role_id = db.Column(db.Integer, nullable=False, foreign_key=True)
+    title = db.Column(db.String(100),)
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+
+#Schemas de las tablas
 
 class TableNameExampleSchema(ma.ModelSchema):
     class Meta:
