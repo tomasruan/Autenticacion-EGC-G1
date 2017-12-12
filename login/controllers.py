523fce2601311fc151c8bf30373eab90e0fa8555
Alejandro Hall <alejandropolvillo@gmail.com>
2017-12-12 21:14:52 +0100

    Elimina los controladores de prueba para dejar limpio los controladores. Ref #17

--- login/controllers.py
+++ login/controllers.py
@@ -4,27 +4,6 @@ from flask import Flask, redirect, url_for
 
 app = Flask(__name__)
 
-#Example of how to add something to database
-@app.route('/add')
-def add():
-    schema = TableNameExampleSchema()
-
-    prueba = TableNameExample(title='Titulo')
-
-    db.session.add(prueba)
-    db.session.commit()
-
-    return schema.dump(prueba).data
-
-
-#Example of how to retrieve a table from database and return it as json
-@app.route('/prueba')
-def root():
-    prueba_schema = TableNameExampleSchema(many=True)
-    prueba = TableNameExample.query.all()
-
-    return prueba_schema.dump(prueba).data
-
 
 if __name__ == '__main__':
     app.run()
