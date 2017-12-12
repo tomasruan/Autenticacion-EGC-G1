9d9413d611978d7252ff8e896b9632cff211d1b1
caolin333 <raulrp1996@gmail.com>
2017-12-12 13:55:09 +0100

    Añade endpoint para asignar roles. Ref #10

--- login/controllers.py
+++ login/controllers.py
@@ -1,5 +1,8 @@
 from database import db, TableNameExample, TableNameExampleSchema
 from flask import Flask, redirect, url_for
+from database import Role, RoleSchema, User_account
+from flask.json import jsonify
+from flask import request
 
 
 app = Flask(__name__)
@@ -25,6 +28,28 @@ def root():
 
     return prueba_schema.dump(prueba).data
 
+@app.route('/assignRole/<int:id>/<int:role_id>')
+def assign_role(id, role_id):
+    roleNumber_id = role_id
+    users = User_account.query.all()
+    print(len(users))
+    user = User_account.query.get(id)
+    if user is not None:
+        user.role_id = roleNumber_id
+        db.session.commit()
+        user_account_assigned={"id":user.id,
+                           "username":user.username,
+                           "password":user.password,
+                           "email":user.email,
+                           "role_id":roleNumber_id}
+        res= {"codigo":1,
+              "status":"Role cambiado",
+              "usuario":user_account_assigned,}
+    else:
+        res= {"codigo":0,
+              "status":"Id de usuario incorrecta, no existe en la base de datos",
+              "usuario":None,}
+    return jsonify(assignRole=res)
 
 if __name__ == '__main__':
     app.run()
