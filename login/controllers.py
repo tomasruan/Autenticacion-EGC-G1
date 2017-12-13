b8799aef2d2e930e2ed83e5c6db57f74932c4575
caolin333 <raulrp1996@gmail.com>
2017-12-13 01:38:06 +0100

    Cambio de metodo HTTP pero aun no terminado. Ref #10

--- login/controllers.py
+++ login/controllers.py
@@ -28,18 +28,17 @@ def root():
 
     return prueba_schema.dump(prueba).data
 
-@app.route('/users/<int:user_id>/role', methods=['PUT'])
+@app.route('/users/<int:user_id>/role', methods=['PATCH'])
 def assign_role(user_id):
-    user = User_account.query.get(user_id).first()
+    user = User_account.query.get(user_id)
     if user is not None:
-        user_account_assigned={"id":user.id,
-                   "username":user.username,
-                   "password":user.password,
-                   "email":user.email,
-                   "role_id":request.json.get('role_id', user['role_id'])}
-        res = {"codigo":1,
-            "status":"Role cambiado",
-            "usuario":user_account_assigned,}
+        if request.json.get('role_id') == 1 | 2:
+            user.role_id = request.json.get('role_id')
+            print(request.json.get('role_id'))
+            print(user.role_id)
+            res = {"codigo":1,
+                "status":"Role cambiado",
+                "usuario":user,}
     else:
         res = {"codigo":0,
             "status":"Id de usuario incorrecta, no existe en la base de datos",
