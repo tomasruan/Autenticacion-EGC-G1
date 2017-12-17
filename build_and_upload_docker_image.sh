514fde23a076932aaa0f2203ec416d94e086eafa
Alejandro Hall <alejandropolvillo@gmail.com>
2017-12-17 01:21:01 +0100

    Revert "Añade error intencionado para ver si falla la build de docker en travis. Ref #30"
    This reverts commit 9962534e2fb2e2ee4e4a250f597a968ce1fd7ffd.

--- build_and_upload_docker_image.sh
+++ build_and_upload_docker_image.sh
@@ -6,6 +6,6 @@ export DOCKER_ID_USER=$DOCKER_USER
 
 docker login --username=$DOCKER_USER --password=$DOCKER_PASSWORD
 
-docker tag login_egc $DOCKER_ID_USER/login_ec
+docker tag login_egc $DOCKER_ID_USER/login_egc
 
 docker push $DOCKER_ID_USER/login_egc
