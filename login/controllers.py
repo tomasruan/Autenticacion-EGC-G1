df8ffa020d1d2e3de49631c833a8143cdb86969f
Manuel <manuelgomezsuarez@gmail.com>
2018-01-10 20:10:53 +0100

    Añade comprobación del captcha en el metodo GET y POST del login. Ref #43

--- login/controllers.py
+++ login/controllers.py
@@ -12,22 +12,29 @@ db.init_app(app)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    captcha="1+2+3"
     if request.method == 'GET':
         cookie = Cookie.query.filter_by(number_id=request.cookies.get('session_id')).first()
         if cookie is None:
-            return render_template('login.html')
+            return render_template('login.html',captcha=captcha)
         else:
             return redirect('/')
 
     elif request.method == 'POST':
         form_username = request.form['username']
         form_password = request.form['password']
+        form_captcha  = request.form['captcha']
 
-        if not validate(form_username, form_password):
-            return render_template('login.html', error='No deje ningún campo en blanco')
+        if not validate(form_username, form_password,form_captcha):
+            return render_template('login.html',captcha=captcha, error='No deje ningún campo en blanco')
 
         user_from_db = User_account.query.filter_by(username=form_username).first()
-
+        try:
+            eval(form_captcha)
+            if(eval(form_captcha) !=eval(captcha)):
+                return render_template('login.html',captcha=captcha, error='Error en el captcha, revise la operación')
+        except:
+            return render_template('login.html', captcha=captcha, error='Error en el captcha, revise la operación')
         if user_from_db is not None:
             if phpass.verify(form_password, user_from_db.password):
                 saved_cookie = create_cookie_and_save(user_from_db)
@@ -37,9 +44,9 @@ def login():
 
                 return response
             else:
-                return render_template('login.html', error='La contraseña no coincide con la del usuario')
+                return render_template('login.html',captcha=captcha ,error='La contraseña no coincide con la del usuario')
         else:
-            return render_template('login.html', error='No existe el usuario')
+            return render_template('login.html', captcha=captcha,error='No existe el usuario')
 
 
 def create_cookie_and_save(user):
